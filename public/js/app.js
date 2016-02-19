a28101a81e2378442dd3c16a1d011158200b0752
Taylor Howard <taylor@ufl.edu>
2016-02-18 19:49:12 -0500

    updated

--- public/js/app.js
+++ public/js/app.js
@@ -1,38 +1,44 @@
-var weatherData = null;
 
 $(document).ready(function(){
   var baseUrl = 'https://api.forecast.io/forecast/';
-  var name = taylor;
+  var name = 'taylor';
 	
-  $('#get-weather').on('click', getWeather);
-
-
+  $('#get-weather').on('click', showInfo);
 
   function buildUrl(lat, lon){
     return baseUrl + apiKey+'/'+lat+','+lon;
   }
-
-  function getWeather(){
-    var lat = $('#latitude').val();
-    var lon = $('#longitude').val();
-    var options = {
-      url: buildUrl(lat, lon),
-      dataType: 'jsonp',
-      success: successHandler,
-      error: errorHandler
-    };
-
-    $.ajax(options);
-  }
-
-
-  function successHandler(data){
-	 weatherData = data;
-    $('#output').text(JSON.stringify(data));
-    console.log(data);
-  }
-
-  function errorHandler(err){
+	
+	function showInfo(){
+		console.log('jkdshkjfj')
+		var lat = $('#latitude').val();
+		var lon = $('#longitude').val();
+		var ajaxObjects = {
+			url: buildUrl (lat,lon),
+			dataType: 'jsonp',
+			success: successRoute,
+			error: errorHandler,
+		};
+		
+		$.ajax(ajaxObjects);
+	}
+	
+	function successRoute(wdata){
+		console.log(wdata);
+		var source = $('#info').html();
+		var template = Handlebars.compile(source);
+		var dataa = {
+			latitude: wdata.latitude,
+			longitude: wdata.longitude,
+			icon: wdata.currently.icon || 'clear night',
+			time: wdata.currently.time,
+			summary: wdata.currently.summary,
+		};
+		var html = template(dataa);
+		$('#output').html(html);
+	}
+	
+	 function errorHandler(err){
     console.log(err);
   }
 });
